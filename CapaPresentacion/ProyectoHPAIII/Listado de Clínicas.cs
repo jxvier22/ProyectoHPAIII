0892478746c95b6d104574e6d2b2826339ae72b4
Javier Espinosa <javier.espinosa2@utp.ac.pa>
2025-10-13 20:17:29 -0500

    Cambios en el listado de clinicas

--- CapaPresentacion/ProyectoHPAIII/Listado de Clínicas.cs
+++ CapaPresentacion/ProyectoHPAIII/Listado de Clínicas.cs
@@ -16,5 +16,10 @@ namespace CapaPresentación
         {
             InitializeComponent();
         }
+
+        private void Pantalla14_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
