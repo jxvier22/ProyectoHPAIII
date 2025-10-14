03706095de4b37d2de87cb85c03c67481e51bcaa
Abdullahpat20 <abdullahpatel2000@gmail.com>
2025-10-14 01:22:32 -0500

    Terminé mi parte

--- CapaPresentacion/ProyectoHPAIII/Login.Designer.cs
+++ CapaPresentacion/ProyectoHPAIII/Login.Designer.cs
@@ -28,7 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            checkBox1 = new CheckBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = CapaPresentación.Properties.Resources.plant_aesthetic_desktop_ybxs8nd06tkc0whu;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(615, -2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(665, 725);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Mulish", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(107, 146);
+            label1.Name = "label1";
+            label1.Size = new Size(372, 47);
+            label1.TabIndex = 1;
+            label1.Text = "Bienvenido de Vuelta";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Mulish", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(92, 193);
+            label2.Name = "label2";
+            label2.Size = new Size(403, 26);
+            label2.TabIndex = 2;
+            label2.Text = "Ingrese sus datos para acceder a su cuenta";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Mulish", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(76, 305);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 22);
+            label3.TabIndex = 3;
+            label3.Text = "Usuario:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(78, 333);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(417, 33);
+            textBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Mulish", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(76, 390);
+            label4.Name = "label4";
+            label4.Size = new Size(90, 22);
+            label4.TabIndex = 5;
+            label4.Text = "Password:";
+            label4.Click += label4_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(78, 415);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(417, 33);
+            textBox2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Mulish", 8.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Blue;
+            label5.Location = new Point(367, 451);
+            label5.Name = "label5";
+            label5.Size = new Size(128, 17);
+            label5.TabIndex = 7;
+            label5.Text = "Olvidé mi contraseña";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            checkBox1.Location = new Point(78, 470);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(150, 19);
+            checkBox1.TabIndex = 8;
+            checkBox1.Text = "Recordarme por 30 dias";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.SteelBlue;
+            button1.FlatAppearance.BorderColor = Color.SteelBlue;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Mulish", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(76, 546);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(419, 39);
+            button1.TabIndex = 32;
+            button1.Text = "Iniciar Sesión";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
             // 
             // Pantalla2
             // 
@@ -36,12 +153,35 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(1280, 720);
+            Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Pantalla2";
             Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label5;
+        private CheckBox checkBox1;
+        private Button button1;
     }
 }
--- CapaPresentacion/ProyectoHPAIII/MenuPrincipla.Designer.cs
+++ CapaPresentacion/ProyectoHPAIII/MenuPrincipla.Designer.cs
@@ -31,12 +31,25 @@
             panel1 = new Panel();
             panel2 = new Panel();
             botonTratamientos = new Button();
-            botonMedicos = new Button();
-            botonAdministradores = new Button();
             botonMenu = new Button();
-            botonClinicas = new Button();
+            botonMedicos = new Button();
             botonPacientes = new Button();
+            botonAdministradores = new Button();
+            botonClinicas = new Button();
+            label1 = new Label();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            label2 = new Label();
+            label3 = new Label();
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            label4 = new Label();
+            label5 = new Label();
             panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -58,7 +71,7 @@
             panel2.Dock = DockStyle.Left;
             panel2.Location = new Point(0, 100);
             panel2.Name = "panel2";
-            panel2.Size = new Size(200, 620);
+            panel2.Size = new Size(236, 620);
             panel2.TabIndex = 1;
             // 
             // botonTratamientos
@@ -67,79 +80,169 @@
             botonTratamientos.BackgroundImageLayout = ImageLayout.None;
             botonTratamientos.FlatAppearance.BorderColor = Color.White;
             botonTratamientos.FlatStyle = FlatStyle.Flat;
+            botonTratamientos.Font = new Font("Mulish", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             botonTratamientos.ForeColor = Color.Black;
-            botonTratamientos.Location = new Point(39, 314);
+            botonTratamientos.Location = new Point(41, 295);
             botonTratamientos.Name = "botonTratamientos";
-            botonTratamientos.Padding = new Padding(1);
-            botonTratamientos.Size = new Size(120, 43);
+            botonTratamientos.Padding = new Padding(1, 1, 1, 1);
+            botonTratamientos.Size = new Size(155, 43);
             botonTratamientos.TabIndex = 17;
             botonTratamientos.Text = "Tratamientos";
             botonTratamientos.UseVisualStyleBackColor = false;
-            // 
-            // botonMedicos
-            // 
-            botonMedicos.BackColor = Color.White;
-            botonMedicos.FlatAppearance.BorderColor = Color.White;
-            botonMedicos.FlatStyle = FlatStyle.Flat;
-            botonMedicos.Location = new Point(39, 118);
-            botonMedicos.Name = "botonMedicos";
-            botonMedicos.Padding = new Padding(10);
-            botonMedicos.Size = new Size(120, 43);
-            botonMedicos.TabIndex = 13;
-            botonMedicos.Text = "Medicos";
-            botonMedicos.UseVisualStyleBackColor = false;
-            // 
-            // botonAdministradores
-            // 
-            botonAdministradores.BackColor = Color.White;
-            botonAdministradores.FlatAppearance.BorderColor = Color.White;
-            botonAdministradores.FlatStyle = FlatStyle.Flat;
-            botonAdministradores.Location = new Point(39, 265);
-            botonAdministradores.Name = "botonAdministradores";
-            botonAdministradores.Padding = new Padding(1);
-            botonAdministradores.Size = new Size(120, 43);
-            botonAdministradores.TabIndex = 16;
-            botonAdministradores.Text = "Administradores";
-            botonAdministradores.UseVisualStyleBackColor = false;
             // 
             // botonMenu
             // 
             botonMenu.BackColor = Color.White;
             botonMenu.FlatAppearance.BorderColor = Color.White;
             botonMenu.FlatStyle = FlatStyle.Flat;
-            botonMenu.Location = new Point(39, 69);
+            botonMenu.Font = new Font("Mulish", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            botonMenu.Location = new Point(57, 70);
             botonMenu.Name = "botonMenu";
-            botonMenu.Padding = new Padding(10);
-            botonMenu.Size = new Size(120, 43);
+            botonMenu.Padding = new Padding(10, 10, 10, 10);
+            botonMenu.Size = new Size(120, 57);
             botonMenu.TabIndex = 12;
             botonMenu.Text = "Menú";
             botonMenu.UseVisualStyleBackColor = false;
             // 
-            // botonClinicas
+            // botonMedicos
             // 
-            botonClinicas.BackColor = Color.White;
-            botonClinicas.FlatAppearance.BorderColor = Color.White;
-            botonClinicas.FlatStyle = FlatStyle.Flat;
-            botonClinicas.Location = new Point(39, 216);
-            botonClinicas.Name = "botonClinicas";
-            botonClinicas.Padding = new Padding(10);
-            botonClinicas.Size = new Size(120, 43);
-            botonClinicas.TabIndex = 15;
-            botonClinicas.Text = "Clinicas";
-            botonClinicas.UseVisualStyleBackColor = false;
+            botonMedicos.BackColor = Color.White;
+            botonMedicos.FlatAppearance.BorderColor = Color.White;
+            botonMedicos.FlatStyle = FlatStyle.Flat;
+            botonMedicos.Font = new Font("Mulish", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            botonMedicos.Location = new Point(58, 115);
+            botonMedicos.Name = "botonMedicos";
+            botonMedicos.Padding = new Padding(10, 10, 10, 10);
+            botonMedicos.Size = new Size(120, 54);
+            botonMedicos.TabIndex = 13;
+            botonMedicos.Text = "Medicos";
+            botonMedicos.UseVisualStyleBackColor = false;
             // 
             // botonPacientes
             // 
             botonPacientes.BackColor = Color.White;
             botonPacientes.FlatAppearance.BorderColor = Color.White;
             botonPacientes.FlatStyle = FlatStyle.Flat;
-            botonPacientes.Location = new Point(39, 167);
+            botonPacientes.Font = new Font("Mulish", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            botonPacientes.Location = new Point(58, 162);
             botonPacientes.Name = "botonPacientes";
-            botonPacientes.Padding = new Padding(10);
-            botonPacientes.Size = new Size(120, 43);
+            botonPacientes.Padding = new Padding(10, 10, 10, 10);
+            botonPacientes.Size = new Size(120, 54);
             botonPacientes.TabIndex = 14;
             botonPacientes.Text = "Pacientes";
             botonPacientes.UseVisualStyleBackColor = false;
+            botonPacientes.Click += botonPacientes_Click;
+            // 
+            // botonAdministradores
+            // 
+            botonAdministradores.BackColor = Color.White;
+            botonAdministradores.FlatAppearance.BorderColor = Color.White;
+            botonAdministradores.FlatStyle = FlatStyle.Flat;
+            botonAdministradores.Font = new Font("Mulish", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            botonAdministradores.Location = new Point(28, 252);
+            botonAdministradores.Name = "botonAdministradores";
+            botonAdministradores.Padding = new Padding(1, 1, 1, 1);
+            botonAdministradores.Size = new Size(175, 43);
+            botonAdministradores.TabIndex = 16;
+            botonAdministradores.Text = "Administradores";
+            botonAdministradores.UseVisualStyleBackColor = false;
+            // 
+            // botonClinicas
+            // 
+            botonClinicas.BackColor = Color.White;
+            botonClinicas.FlatAppearance.BorderColor = Color.White;
+            botonClinicas.FlatStyle = FlatStyle.Flat;
+            botonClinicas.Font = new Font("Mulish", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            botonClinicas.Location = new Point(57, 207);
+            botonClinicas.Name = "botonClinicas";
+            botonClinicas.Padding = new Padding(10, 10, 10, 10);
+            botonClinicas.Size = new Size(120, 53);
+            botonClinicas.TabIndex = 15;
+            botonClinicas.Text = "Clinicas";
+            botonClinicas.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Mulish", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(286, 129);
+            label1.Name = "label1";
+            label1.Size = new Size(190, 44);
+            label1.TabIndex = 2;
+            label1.Text = "Dashboard";
+            label1.Click += label1_Click;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(label2);
+            panel3.Location = new Point(297, 176);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(338, 173);
+            panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(label3);
+            panel4.Location = new Point(785, 176);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(338, 173);
+            panel4.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Mulish Medium", 20F, FontStyle.Bold);
+            label2.Location = new Point(13, 122);
+            label2.Name = "label2";
+            label2.Size = new Size(138, 37);
+            label2.TabIndex = 0;
+            label2.Text = "Doctores";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Mulish Medium", 20F, FontStyle.Bold);
+            label3.Location = new Point(19, 122);
+            label3.Name = "label3";
+            label3.Size = new Size(148, 37);
+            label3.TabIndex = 1;
+            label3.Text = "Pacientes";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(297, 424);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(394, 248);
+            dataGridView1.TabIndex = 5;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(785, 424);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(394, 248);
+            dataGridView2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Mulish Medium", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(299, 394);
+            label4.Name = "label4";
+            label4.Size = new Size(208, 26);
+            label4.TabIndex = 7;
+            label4.Text = "Medicos Disponibles";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Mulish Medium", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(785, 395);
+            label5.Name = "label5";
+            label5.Size = new Size(204, 26);
+            label5.TabIndex = 8;
+            label5.Text = "Pacientes Recientes";
             // 
             // MenuPrincipla
             // 
@@ -147,13 +250,27 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(1280, 720);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(label1);
             Controls.Add(panel2);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "MenuPrincipla";
             Text = "Form1";
             panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -166,5 +283,14 @@
         private Button botonPacientes;
         private Button botonAdministradores;
         private Button botonClinicas;
+        private Label label1;
+        private Panel panel3;
+        private Label label2;
+        private Panel panel4;
+        private Label label3;
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private Label label4;
+        private Label label5;
     }
 }
