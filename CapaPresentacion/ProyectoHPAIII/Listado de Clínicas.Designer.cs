f9eaa2b6eb5498d74c858e1d446c7fbb11293761
Javier Espinosa <javier.espinosa2@utp.ac.pa>
2025-10-13 20:37:44 -0500

    Cambios en el diseño de pantalla Clinicas

--- CapaPresentacion/ProyectoHPAIII/Listado de Clínicas.Designer.cs
+++ CapaPresentacion/ProyectoHPAIII/Listado de Clínicas.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            label1 = new Label();
             SuspendLayout();
             // 
             // panel1
@@ -37,23 +40,59 @@
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(800, 72);
+            panel1.Size = new Size(1051, 89);
             panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 89);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(225, 476);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Dock = DockStyle.Bottom;
+            panel3.Location = new Point(225, 525);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(826, 40);
+            panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
+            label1.Location = new Point(225, 89);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 3;
+            label1.Text = "label1";
             // 
             // Pantalla14
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1051, 565);
+            Controls.Add(label1);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
             Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Pantalla14";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Pantalla14";
             Load += Pantalla14_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Label label1;
     }
 }
